c4edbad54e22ddd2727e3324c0846a63491d1064
Raudhil <raudhilfirdausn@gmail.com>
2025-02-19 09:03:21 +0700

    Jobsheet-1 Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,43 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Selamat');
+});
+
+Route::get('/', function () {
+    return ('Selamat Datang');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    $nama = "Raudhil Firdaus Naufal";
+    $nim = "2341720164";
+    return 'Nama: ' . $nama . ', NIM: ' . $nim;
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Ini halaman artikel ke-' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
 });
